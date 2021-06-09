611234b982d20acd7d881c45a4db9113f8b6e3d5
unknown <jardelinomedeiros@outlook.com>
2021-06-09 17:25:57 -0300

    feat(responsive navbar):creating responsive navbar

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+// toggle menu responsivo
+let btnShowMenu = document.getElementById('icon-bars');
+
+btnShowMenu.addEventListener('click', showMenu);
+
+//função que mostra e esconde o menu 
+function showMenu() {
+    
+    let navbarList = document.querySelector(".header__nav ul");
+    
+    
+    if(navbarList) {
+        navbarList.classList.toggle("visible");
+    }
+}
+
+
+
+
