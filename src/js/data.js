78262ace8aa54c662abf9c9cb2f24d51277dcdaa
unknown <jardelinomedeiros@outlook.com>
2021-06-20 18:43:02 -0300

    cards data config

--- src/js/data.js
+++ src/js/data.js
@@ -1,4 +1,4 @@
-let image1 = document.getElementById("img").src="/src/img/mojibullah-shahir-SXeu2Cps5vM-unsplash.jpg";
+let image1 = document.getElementById("img").src="./src/img/marc-olivier-jodoin-BG9oZ15a4Xk-unsplash.jpg";
 
 const posts = [
     {
@@ -8,25 +8,40 @@ const posts = [
         text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, corrupti. Reprehenderit autem rerum."
     },
     {
-        id: 1,
+        id: 2,
         image: image1,
-        title: "title",
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, corrupti. Reprehenderit autem rerum."
-    }
+        title: "title 2",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, corrupti. Reprehenderit autem rerum 2."
+    },
+    {
+        id: 2,
+        image: image1,
+        title: "title 3",
+        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore, corrupti. Reprehenderit autem rerum 2."
+    },
+    
 ];
 
-    let imagem = document.getElementById("img");
-    let titulo = document.getElementById("title"); 
-    let texto = document.getElementById("text");
+    let card = document.getElementById("card");
+    
+    card.innerHTML = " "; 
+   
+    for( i = 0; i < posts.length; i++) {
+        card.innerHTML += `
+            <section>
+                <div class="article__card" >
+                    
+                <img id="img" src="${posts[i].image}">
 
-    titulo.innerHTML = "";
-    texto.innerHTML = "";
-    imagem.innerHTML = "";
+                    <div>
+                        <h3>  ${posts[i].title}  </h3>
+                        <p> ${posts[i].text} </p>
+                    </div>
+                
+                <div>
+            </section>
+        `
+    }
 
-   for(let i=0; i< posts.length; i++) {
-        titulo.innerHTML += "<h3>"+posts[i].title+"</h3>";
-        texto.innerHTML += "<p>"+posts[i].text+"</p>";
-        imagem.innerHTML += "<img src="+posts[i].imagem+"/>"
-   }
 
-
+  
